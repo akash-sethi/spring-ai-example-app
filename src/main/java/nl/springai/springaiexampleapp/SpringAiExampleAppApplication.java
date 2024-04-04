95e2efe4c44f97926b33709b5068c5767fdb0fc2
akashsethi <akashsethiwork@gmail.com>
2024-04-04 20:37:49 +0200

    app init

---
+++ src/main/java/nl/springai/springaiexampleapp/SpringAiExampleAppApplication.java
@@ -0,0 +1,13 @@
+package nl.springai.springaiexampleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiExampleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiExampleAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/springai/springaiexampleapp/SpringAiExampleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.springai.springaiexampleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiExampleAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
